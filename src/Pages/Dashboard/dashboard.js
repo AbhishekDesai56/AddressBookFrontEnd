513b7bc0c5a42a11077f316be8e03731dd404af9
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-02 23:12:30 +0530

    refactor: added dashboard pages with css

---
+++ src/Pages/Dashboard/dashboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./dashboard.scss";
+import AddressBookList from "../../Components/AddressBookList";
+const Dashboard = (props) => {
+  return (
+    <div class="main-content">
+      <div class="header-content">
+        <div class="person-detail-text">Person Details</div>
+        <a href="../pages/address_form.html" class="add-button">
+          Add Person
+        </a>
+      </div>
+      <AddressBookList />
+    </div>
+  );
+};
+
+export default Dashboard;
