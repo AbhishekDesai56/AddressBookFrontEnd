5050e4767cf1ecdded1f5c23e04f98755b317c6f
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-02 07:20:53 +0530

    feat: added login and register  form with yup validation.

--- src/Pages/Register/register.js
+++ src/Pages/Register/register.js
@@ -2,7 +2,8 @@ import React from "react";
 import { withFormik } from "formik";
 import * as Yup from "yup";
 import Input from "../../Components/Input";
-const login = (props) => {
+
+const register = (props) => {
   const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
     props;
 
@@ -11,6 +12,40 @@ const login = (props) => {
       <div class="form-head">
         <div class="head-title">
           <span>Login Form</span>
+        </div>
+      </div>
+      <div class="input-name-content">
+        <div class="a">
+          <Input
+            control="input"
+            type="text"
+            label="First Name"
+            name="firstName"
+            placeholder="Your First Name.."
+            className="input"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.firstName}
+          />
+          {errors.firstName && touched.firstName && (
+            <div id="feedback">{errors.firstName}</div>
+          )}
+        </div>
+        <div class="a">
+          <Input
+            control="input"
+            type="text"
+            label="Last Name"
+            name="lastName"
+            placeholder="Your Last Name.."
+            className="input"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.lastName}
+          />
+          {errors.lastName && touched.lastName && (
+            <div id="feedback">{errors.lastName}</div>
+          )}
         </div>
       </div>
       <div class="row-content">
@@ -54,13 +89,23 @@ const login = (props) => {
   );
 };
 
-const MyEnhancedLogin = withFormik({
+const MyEnhancedRegister = withFormik({
   mapPropsToValues: () => ({
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
   }),
 
   validationSchema: Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Firstname is required"),
+    lastName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Lastname is required"),
     email: Yup.string()
       .email("Must be a valid email")
       .max(255)
@@ -76,6 +121,6 @@ const MyEnhancedLogin = withFormik({
   },
 
   displayName: "BasicForm",
-})(login);
+})(register);
 
-export default MyEnhancedLogin;
+export default MyEnhancedRegister;
