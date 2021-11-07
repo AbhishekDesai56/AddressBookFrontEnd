8631c613aebec9f3ff888d817f58c5f725a1a932
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-08 03:47:12 +0530

    feat: added test cases for address form

---
+++ src/Pages/AddressForm/addressForm.test.js
@@ -0,0 +1,99 @@
+import Enzyme, { mount, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import MyEnhancedAddressForm from "./addressForm";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("MyEnhancedAddressForm", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<MyEnhancedAddressForm />);
+  });
+
+  it("shows my default text", () => {
+    expect(wrapper.find("span").text()).toEqual("Person Address Form");
+  });
+
+  it("fill the form with values", () => {
+    const addressFormDetails = {
+      firstName: "leon",
+      lastName: "ban",
+      address: "mumbai mumbai",
+      city: "mumbai",
+      pinCode: "400001",
+      phoneNumber: "9999999999",
+    };
+
+    const firstNameInput = wrapper.find("#firstName");
+    firstNameInput.value = addressFormDetails.firstName;
+    expect(firstNameInput.value).toBe("leon");
+
+    const lastNameInput = wrapper.find("#lastName");
+    lastNameInput.value = addressFormDetails.lastName;
+    expect(lastNameInput.value).toBe("ban");
+
+    const addressInput = wrapper.find("#address");
+    addressInput.value = addressFormDetails.address;
+    expect(addressInput.value).toBe("mumbai mumbai");
+
+    const cityInput = wrapper.find("#city");
+    cityInput.value = addressFormDetails.city;
+    expect(cityInput.value).toBe("mumbai");
+
+    const pinCodeInput = wrapper.find("#pinCode");
+    pinCodeInput.value = addressFormDetails.pinCode;
+    expect(pinCodeInput.value).toBe("400001");
+
+    const phoneNumberInput = wrapper.find("#phoneNumber");
+    phoneNumberInput.value = addressFormDetails.phoneNumber;
+    expect(phoneNumberInput.value).toBe("9999999999");
+  });
+});
+
+describe("The components are rendered", () => {
+  it("renders Login component without crashing", () => {
+    shallow(<MyEnhancedAddressForm />);
+  });
+  it("renders title without crashing", () => {
+    const wrapper = mount(<MyEnhancedAddressForm />);
+    const header = <span>Person Address Form</span>;
+    expect(wrapper.contains(header)).toBe(true);
+  });
+  it("renders form inputs", () => {
+    const wrapper = mount(<MyEnhancedAddressForm />);
+    expect(wrapper.find('input[id="firstName"]')).toHaveLength(1);
+    expect(wrapper.find('input[id="lastName"]')).toHaveLength(1);
+    expect(wrapper.find('textarea[id="address"]')).toHaveLength(1);
+    expect(wrapper.find('select[id="city"]')).toHaveLength(1);
+    expect(wrapper.find('select[id="pinCode"]')).toHaveLength(1);
+    expect(wrapper.find('input[id="phoneNumber"]')).toHaveLength(1);
+  });
+  it("renders submit button without crashing", () => {
+    const wrapper = mount(<MyEnhancedAddressForm />);
+    const label = wrapper.find("#submitButton").text();
+    expect(label).toBe("Add");
+    const label2 = wrapper.find("#resetButton").text();
+    expect(label2).toBe("Reset");
+  });
+});
+
+describe("The events are working", () => {
+  it("The form is submitted when the click event is fired by simulated click on the submit button", () => {
+    const mockCallBack = jest.fn();
+
+    const wrapper = mount(<MyEnhancedAddressForm onSubmit={mockCallBack()} />);
+
+    wrapper.find("#submitButton").simulate("click");
+    expect(mockCallBack).toHaveBeenCalledTimes(1);
+
+    wrapper.find("#resetButton").simulate("click");
+    expect(mockCallBack).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("Snapshot", () => {
+  it("matches App the snapshot", () => {
+    const wrapper = mount(<MyEnhancedAddressForm />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
