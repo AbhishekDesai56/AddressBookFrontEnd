46d8c817084b90dcb16d26c6d8be4f966b102f60
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-08 04:04:27 +0530

    feat: added address book service with http common file

---
+++ src/Services/AddressBookService.js
@@ -0,0 +1,43 @@
+import http from "../http-common.js";
+let token = sessionStorage.getItem("token");
+
+let header = {
+  headers: { token: token },
+};
+
+class AddressBookService {
+  login(data) {
+    return http.post("/login", data);
+  }
+
+  register(data) {
+    return http.post("/saveUserData", data);
+  }
+
+  getAllAddressBookData = () => {
+    token = sessionStorage.getItem("token");
+    header = {
+      headers: { token: token },
+    };
+    return http.get("/getAddressBookData", header);
+  };
+
+  getAddressBookById = (id) => {
+    id = id || "";
+    return http.get(`/getAddressBookData/${id}`, header);
+  };
+
+  createAddressBookData = (data) => {
+    return http.post("/saveAddressBookData", data, header);
+  };
+
+  updateAddressBookId = (id, data) => {
+    return http.put(`/updateAddressBookData/${id}`, data, header);
+  };
+
+  deleteAddressBookId = (id) => {
+    return http.delete(`/deleteAddressBookData/${id}`, header);
+  };
+}
+
+export default new AddressBookService();
