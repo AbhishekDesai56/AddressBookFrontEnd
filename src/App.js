6bd6b07eaac0bc4ff2492378fde5a1d20e8b2b16
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-11 08:53:04 +0530

    refactor: added routes from addressbook dashboard, addform along with protected route.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MyEnhancedAddressForm from "../src/Pages/AddressForm/addressForm";
+import Login from "../src/Pages/Login/login";
+import MyEnhancedRegister from "../src/Pages/Register/register";
+import Dashboard from "../src/Pages/Dashboard/dashboard";
+import AddressForm from "../src/Pages/AddressForm/addressForm";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ProtectedRoute } from "../src/Components/ProtectedRoute";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MyEnhancedAddressForm />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/register/" exact component={MyEnhancedRegister}></Route>
+          <ProtectedRoute
+            path="/details/"
+            exact
+            component={AddressForm}
+          ></ProtectedRoute>
+          <ProtectedRoute
+            path="/details/:id"
+            exact
+            component={AddressForm}
+          ></ProtectedRoute>
+          <ProtectedRoute
+            path="/dashboard/"
+            exact
+            component={Dashboard}
+          ></ProtectedRoute>
+          <Route path="/" exact component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 export default App;
