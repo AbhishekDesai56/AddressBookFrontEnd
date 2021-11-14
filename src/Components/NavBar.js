95db3a2626c58cf20cba52463e11ac15cc6460af
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-15 02:08:25 +0530

    refactor: added logout button

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./NavBar.scss";
 import image from "../assets/logo.png";
+
 const NavBar = (WrappedComponent) => {
+  const logout = () => {
+    sessionStorage.clear();
+  };
+
   return (props) => (
     <>
       <header className="header-content header">
@@ -13,12 +18,39 @@ const NavBar = (WrappedComponent) => {
             <span className="add-text add-book">Book</span>
           </div>
         </div>
+        <ul>
+          {isShow() ? (
+            <a
+              href="/"
+              className="logout-button"
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logout
+            </a>
+          ) : (
+            <span></span>
+          )}
+        </ul>
       </header>
       <div>
         <WrappedComponent {...props} />
       </div>
     </>
   );
+
+  function isShow() {
+    if (window.location.href.indexOf("dashboard") > -1) {
+      console.log("true");
+      return true;
+    } else if (window.location.href.indexOf("details") > -1) {
+      return true;
+    } else {
+      console.log("false");
+      return false;
+    }
+  }
 };
 
 export default NavBar;
