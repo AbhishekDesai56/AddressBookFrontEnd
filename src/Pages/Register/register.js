1d11a7bd00f623ef26f98152e15f534daf236fd3
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-15 02:09:34 +0530

    refactor: redesign structure and added css

--- src/Pages/Register/register.js
+++ src/Pages/Register/register.js
@@ -1,110 +1,26 @@
-import React from "react";
-import { withFormik } from "formik";
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import AddressBookService from "../../Services/AddressBookService";
+import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Input from "../../Components/Input";
 import "./register.scss";
+import NavBar from "../../Components/NavBar";
 
-const register = (props) => {
-  const { values, touched, errors, handleChange, handleBlur, handleSubmit } =
-    props;
+const Register = (props) => {
+  const history = useHistory();
+  const [disable, setDisable] = useState(false);
 
-  return (
-    <form className="register-form" onSubmit={handleSubmit}>
-      <div className="register-form-head">
-        <div className="register-head-title">
-          <span>Register Form</span>
-        </div>
-      </div>
-      <div className="register-row-content">
-        <Input
-          control="input"
-          type="text"
-          label="First Name"
-          name="firstName"
-          placeholder="Your First Name.."
-          className="register-input"
-          id="firstName"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.firstName}
-        />
-        {errors.firstName && touched.firstName && (
-          <div id="error-output">{errors.firstName}</div>
-        )}
-      </div>
-      <div className="register-row-content">
-        <Input
-          control="input"
-          type="text"
-          label="Last Name"
-          name="lastName"
-          placeholder="Your Last Name.."
-          className="register-input"
-          id="lastName"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.lastName}
-        />
-        {errors.lastName && touched.lastName && (
-          <div id="error-output">{errors.lastName}</div>
-        )}
-      </div>
-      <div className="register-row-content">
-        <Input
-          type="email"
-          label="Email"
-          name="email"
-          placeholder="Enter your Email"
-          className="register-input"
-          id="email"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.email}
-        />
-        {errors.email && touched.email && (
-          <div id="error-output">{errors.email}</div>
-        )}
-      </div>
-      <div className="register-row-content">
-        <Input
-          type="password"
-          label="Password"
-          name="password"
-          placeholder="Enter your password"
-          className="register-input"
-          id="password"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.password}
-        />
-        {errors.password && touched.password && (
-          <div id="error-output">{errors.password}</div>
-        )}
-      </div>
-      <div className="register-button-content">
-        <div className="register-submit">
-          <button
-            id="submitbutton"
-            type="submit"
-            className="register-button submitButton"
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </form>
-  );
-};
-
-const MyEnhancedRegister = withFormik({
-  mapPropsToValues: () => ({
+  const initialValues = {
     firstName: "",
     lastName: "",
     email: "",
     password: "",
-  }),
+  };
 
-  validationSchema: Yup.object().shape({
+  const validationSchema = Yup.object().shape({
     firstName: Yup.string()
       .min(2, "Too Short!")
       .max(50, "Too Long!")
@@ -113,21 +29,106 @@ const MyEnhancedRegister = withFormik({
       .min(2, "Too Short!")
       .max(50, "Too Long!")
       .required("Lastname is required"),
-    email: Yup.string()
-      .email("Must be a valid email")
-      .max(255)
-      .required("Email is required"),
-    password: Yup.string().max(255).required("Password is required"),
-  }),
+    email: Yup.string().email("Email is required").required("Required"),
+    password: Yup.string()
+      .required("Password is required")
+      .required("Required"),
+  });
 
-  handleSubmit: (values, { setSubmitting }) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 1000);
-  },
+  const onSubmit = (values) => {
+    let registerationData = {
+      firstName: values.firstName,
+      lastName: values.lastName,
+      email: values.email,
+      password: values.password,
+    };
 
-  displayName: "BasicForm",
-})(register);
+    AddressBookService.register(registerationData)
+      .then((response) => {
+        setDisable(true);
+        setTimeout(() => {
+          history.push("/");
+        }, 2000);
+        toast.success(response.data.message);
+      })
+      .catch((err) => {
+        setDisable(false);
+        toast.error(err);
+      });
+  };
 
-export default MyEnhancedRegister;
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <div className="register-form-container">
+        <Form className="register-form">
+          <div className="register-form-head">
+            <div className="register-head-title">
+              <span>Register Form</span>
+            </div>
+          </div>
+          <div className="register-row-content">
+            <Input
+              control="input"
+              type="text"
+              label="First Name"
+              name="firstName"
+              placeholder="Your First Name.."
+              className="register-input"
+              id="firstName"
+            />
+          </div>
+          <div className="register-row-content">
+            <Input
+              control="input"
+              type="text"
+              label="Last Name"
+              name="lastName"
+              placeholder="Your Last Name.."
+              className="register-input"
+              id="lastName"
+            />
+          </div>
+          <div className="register-row-content">
+            <Input
+              type="email"
+              label="Email"
+              name="email"
+              placeholder="Enter your Email"
+              className="register-input"
+              id="email"
+            />
+          </div>
+          <div className="register-row-content">
+            <Input
+              type="password"
+              label="Password"
+              name="password"
+              placeholder="Enter your password"
+              className="register-input"
+              id="password"
+            />
+          </div>
+          <div className="register-button-content">
+            <div className="register-submit">
+              <button
+                id="submitButton"
+                type="submit"
+                className="register-button submitButton"
+                disabled={disable}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+          <ToastContainer />
+        </Form>
+      </div>
+    </Formik>
+  );
+};
+
+export default NavBar(Register);
