ec32f0352031061176cc674a498e236ad2439a93
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-15 00:04:27 +0530

    refactor: added register link with css

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -8,8 +8,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Input from "../../Components/Input";
 import Password from "../../Components/Password";
 import NavBar from "../../Components/NavBar";
-import { ErrorBoundary } from "../../Helpers/ErrorBoundary";
 import "./login.scss";
+
 const Login = () => {
   const history = useHistory();
   const [disable, setDisable] = useState(false);
@@ -49,50 +49,52 @@ const Login = () => {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <ErrorBoundary>
-        <NavBar />
-        <div className="login-form-content">
-          <Form className="login-form">
-            <div className="login-form-head">
-              <div className="login-head-title">
-                <span>Sign in</span>
-              </div>
-              <div className="login-cancel-icon"></div>
+      <div className="login-form-container">
+        <Form className="login-form">
+          <div className="login-form-head">
+            <div className="login-head-title">
+              <span>Sign in</span>
             </div>
-            <div className="login-row-content">
-              <Input
-                label="Email"
-                name="email"
-                placeholder="Your Email"
-                className="login-input"
-              />
+            <div className="login-cancel-icon"></div>
+          </div>
+          <div className="login-row-content">
+            <Input
+              label="Email"
+              name="email"
+              placeholder="Your Email"
+              className="login-input"
+            />
+          </div>
+          <div className="login-row-content">
+            <Password
+              label="Password"
+              name="password"
+              placeholder="Your Password"
+              className="login-input"
+            />
+          </div>
+          <div className="login-row-content">
+            <a className="link-register" href="/register">
+              Register
+            </a>
+          </div>
+          <div className="login-button-content">
+            <div className="login-submit-reset">
+              <button
+                id="submitButton"
+                type="submit"
+                className="login-button submitButton"
+                disabled={disable}
+              >
+                Submit
+              </button>
             </div>
-            <div className="login-row-content">
-              <Password
-                label="Password"
-                name="password"
-                placeholder="Your Password"
-                className="login-input"
-              />
-            </div>
-            <div className="login-button-content">
-              <div className="login-submit-reset">
-                <button
-                  id="submitButton"
-                  type="submit"
-                  className="login-button submitButton"
-                  disabled={disable}
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-            <ToastContainer />
-          </Form>
-        </div>
-      </ErrorBoundary>
+          </div>
+          <ToastContainer />
+        </Form>
+      </div>
     </Formik>
   );
 };
 
-export default Login;
+export default NavBar(Login);
