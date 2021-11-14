bbcb4a720c50a4aeb880f46a4b5df628ca720a4f
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-15 03:37:59 +0530

    refactor: corrected the change name and added error boundary

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import "./App.css";
 import Login from "../src/Pages/Login/login";
-import MyEnhancedRegister from "../src/Pages/Register/register";
+import Register from "../src/Pages/Register/register";
 import Dashboard from "../src/Pages/Dashboard/dashboard";
-import AddressForm from "../src/Pages/AddressForm/addressForm";
+import AddressForm from "./Pages/AddressForm/addEditAddressForm";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ProtectedRoute } from "../src/Components/ProtectedRoute";
+import { ErrorBoundary } from "../src/Helpers/ErrorBoundary";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/register/" exact component={MyEnhancedRegister}></Route>
-          <ProtectedRoute
-            path="/details/"
-            exact
-            component={AddressForm}
-          ></ProtectedRoute>
-          <ProtectedRoute
-            path="/details/:id"
-            exact
-            component={AddressForm}
-          ></ProtectedRoute>
-          <ProtectedRoute
-            path="/dashboard/"
-            exact
-            component={Dashboard}
-          ></ProtectedRoute>
-          <Route path="/" exact component={Login}></Route>
-        </Switch>
-      </div>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/register/" exact component={Register}></Route>
+            <ProtectedRoute
+              path="/details/"
+              exact
+              component={AddressForm}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/details/:id"
+              exact
+              component={AddressForm}
+            ></ProtectedRoute>
+            <ProtectedRoute
+              path="/dashboard/"
+              exact
+              component={Dashboard}
+            ></ProtectedRoute>
+            <Route path="/" exact component={Login}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
-}
+};
 export default App;
