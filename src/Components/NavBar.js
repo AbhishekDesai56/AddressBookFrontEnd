0b92cd98e1f6f63a2ae19789bf143c89906cca9a
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-14 23:46:39 +0530

    refactor: added higher order component code in navbar.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./NavBar.scss";
 import image from "../assets/logo.png";
-const NavBar = (props) => {
-  return (
-    <header className="header-content header">
-      <div className="logo-content">
-        <img src={image} className="logo-content-img" alt="" />
-        <div>
-          <span className="add-text">Address</span>
-          <br />
-          <span className="add-text add-book">Book</span>
+const NavBar = (WrappedComponent) => {
+  return (props) => (
+    <>
+      <header className="header-content header">
+        <div className="logo-content">
+          <img src={image} className="logo-content-img" alt="" />
+          <div>
+            <span className="add-text">Address</span>
+            <br />
+            <span className="add-text add-book">Book</span>
+          </div>
         </div>
+      </header>
+      <div>
+        <WrappedComponent {...props} />
       </div>
-    </header>
+    </>
   );
 };
 
