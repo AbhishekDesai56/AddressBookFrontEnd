164f92253fee5498f9c2e9e1480566d2e52837bd
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-19 19:39:02 +0530

    refactor: added type property in input

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { ErrorMessage, Field } from "formik";
 const Input = (props) => {
-  const { label, name, placeholder, ...rest } = props;
+  const { type, label, name, placeholder, ...rest } = props;
   return (
     <>
       <label htmlFor={name} className="label text">
         {label}
       </label>
-      <Field type="text" name={name} placeholder={placeholder} {...rest} />
+      <Field type={type} name={name} placeholder={placeholder} {...rest} />
       <span id="error-output">
         <ErrorMessage name={name} />
       </span>
