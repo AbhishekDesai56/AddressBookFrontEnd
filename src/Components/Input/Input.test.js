f5145dd6b6c78d9afe299f27cc4a5b054addb1ab
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-11-24 00:45:27 +0530

    feat: added input component test cases

---
+++ src/Components/Input/Input.test.js
@@ -0,0 +1,95 @@
+import React from "react";
+import Enzyme, { mount, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Input from "./Input";
+import { ErrorMessage } from "formik";
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<Input />", function testsRelyOnTHIS() {
+  const props = {
+    type: "text",
+    label: "First Name",
+    name: "firstName",
+    placeholder: "Your First Name",
+    onClick: "hello",
+    errorMessage: "Invalid input",
+  };
+
+  // it("renders the provided type text", () => {
+  //   const wrapper = shallow(
+  //     <Input
+  //       type={props.type}
+  //       label={props.label}
+  //       name={props.name}
+  //       placeholder={props.placeholder}
+  //     />
+  //   );
+  //   const node = wrapper.find("type").text();
+  //   expect(node).toBe(props.type);
+  // });
+
+  it("renders the provided label text", () => {
+    const wrapper = shallow(
+      <Input
+        type={props.type}
+        label={props.label}
+        name={props.name}
+        placeholder={props.placeholder}
+      />
+    );
+    const node = wrapper.find("label").text();
+    expect(node).toBe(props.label);
+  });
+
+  it("renders the provided name text", () => {
+    const wrapper = shallow(
+      <Input
+        type={props.type}
+        label={props.label}
+        name={props.name}
+        placeholder={props.placeholder}
+      />
+    );
+    const node = wrapper.find("Field").name();
+    expect(node).toEqual("Field");
+  });
+
+  it("renders the provided name type", () => {
+    const props = {
+      type: "text",
+    };
+    const wrapper = shallow(<Input {...props} />);
+    const node = wrapper.find("Field").prop("type");
+    expect(node).toEqual("text");
+  });
+
+  it("renders the provided of name", () => {
+    const props = {
+      type: "text",
+      name: "firstName",
+    };
+    const wrapper = shallow(<Input {...props} />);
+    const node = wrapper.find("Field").prop("name");
+    expect(node).toEqual("firstName");
+  });
+
+  it("renders the provided of placeholder", () => {
+    const props = {
+      type: "text",
+      name: "firstName",
+      placeholder: "Your First Name",
+    };
+    const wrapper = shallow(<Input {...props} />);
+    const node = wrapper.find("Field").prop("placeholder");
+    expect(node).toEqual("Your First Name");
+  });
+
+  it("renders the provided name type of password", () => {
+    const props = {
+      type: "password",
+    };
+    const wrapper = shallow(<Input {...props} />);
+    const node = wrapper.find("Field").prop("type");
+    expect(node).toEqual("password");
+  });
+});
